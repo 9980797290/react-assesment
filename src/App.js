e6975586cb14a8b3fcb871be4401706e93f3bd7f
jhansi <jhansi12mar2000@gmail.com>
2022-05-10 21:45:15 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// import  Component1  from './Component1'
+
+
+import Outcome from './Outcome'
+
+
+
+const App=()=> {
+return (
+<div>
+ <Outcome/>
+ {/* <Component1/> */}
+ </div>
+  
+ 
+ 
+       
+      
+      
+    
+  )
+}
+export default App
