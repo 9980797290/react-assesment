2890885c8e3a0d8752f7654a4ce776dca5f300df
Jhansi R <97968506+9980797290@users.noreply.github.com>
2022-05-12 09:48:06 +0530

    Update Outcome.js

--- src/Outcome.js
+++ src/Outcome.js
@@ -13,8 +13,8 @@ const Outcome=()=> {
     <center>
     <h1 id="id">Styling using Functional and Class Component</h1>
     </center>
- <button className="btn"onClick={()=>SetName(true)}>To see styling in Functional Component</button>
- <button className="btn1"onClick={()=>SetName2(true)}>To see styling in Class Component</button>
+ <button className="btn"onClick={()=>name?SetName(false):SetName(true)}>To see styling in Functional Component</button>
+ <button className="btn1"onClick={()=>name2?SetName2(false):SetName2(true)}>To see styling in Class Component</button>
  {name&& <Function />}
  {name2&&<Class />}
 </div>
